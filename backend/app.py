abdbc15a30e0a79b0afdbf558f88cad6f94ea064
MathiasB <37438935+BerlancourtMathias@users.noreply.github.com>
2023-11-10 15:15:45 +0100

    update front

--- backend/app.py
+++ backend/app.py
@@ -1,29 +1,30 @@
+
+import os
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from dotenv import load_dotenv, find_dotenv
-import openai
-import os
+from openai import OpenAI
 # DOTENV
 load_dotenv(find_dotenv())
-openai.api_key = os.getenv("OPEN_AI_KEY")
+client = OpenAI(api_key=os.getenv("OPEN_AI_KEY"))
+
 
 # FLASK
 app = Flask(__name__)
-CORS(app)
+cors = CORS(app)
 
 
 @app.route('/api', methods=['POST'])
 def chat():
     data = request.get_json()
     message = data['message']
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=message,
-        max_tokens=1024,
-        temperature=0.9,
-    )
+    response = client.completions.create(model="text-davinci-003",
+                                         prompt=message,
+                                         max_tokens=1024,
+                                         temperature=0.9)
     print(response.choices[0].text)
-    return jsonify({'message': response.choices[0].text})
+    result = jsonify({'message': response.choices[0].text})
+    return result
 
 
 if __name__ == '__main__':
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [inputMsg, setinputMsg] = useState("");
+  const [botMessage, setBotMessage] = useState("");
+  const [userMessage, setUserMessage] = useState("");
+  const [conversation, setConversation] = useState([]);
+  const url = "http://localhost:9000/api";
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const { data } = await axios.post(url, { message: inputMsg });
+
+      const newTabConversation = [...conversation];
+
+      // Use inputMsg and data.message directly
+      if (inputMsg) {
+        newTabConversation.push({ userMessage: inputMsg });
+      }
+      if (data.message) {
+        newTabConversation.push({ botMessage: data.message });
+      }
+
+      setConversation(newTabConversation);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center justify-center h-screen">
+      <div className="border-2 rounded-md border-cyan-800 p-4 w-[90%] md:w-[75%] lg:w-[50%] h-[90%] md:h-[75%] lg:h-[50%] flex flex-col">
+        <div className="border-4 rounded-md border-red-600 justify-between p-4 h-full overflow-y-scroll">
+          {conversation.map((message, index) => (
+            <div>
+              {message.userMessage && (
+                <div key={index} className="chat chat-end">
+                  <div className="chat-bubble chat-bubble-info">
+                    {message.userMessage}
+                  </div>
+                </div>
+              )}
+              {message.botMessage && (
+                <div key={index} className="chat chat-start">
+                  <div className="chat-bubble chat-bubble-accent">
+                    {message.botMessage}
+                  </div>
+                </div>
+              )}
+            </div>
+          ))}
+          {/* <div className="chat chat-start">
+            {botMessage && (
+              <div className="chat-bubble chat-bubble-accent">{botMessage}</div>
+            )}
+          </div> */}
+        </div>
+        <form onSubmit={handleSubmit} className="space-x-4 p-4">
+          <input
+            type="text"
+            className="w-full md:w-[80%]"
+            placeholder="Enter your message ..."
+            value={inputMsg}
+            onChange={(e) => setinputMsg(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="p-2 bg-blue-700 rounded-md w-full md:w-[15%] text-white"
+          >
+            Send
+          </button>
+        </form>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
